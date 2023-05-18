ec2c90977ed2d14a2804ce1dced40d0ae10ff492
zhyunk <gimwlgus@gmail.com>
2023-05-19 04:03:26 +0900

    AccountLock 명세 파일 주석 수정
    어노테이션에 관한 주석과, 
    메서드의 기본값에 대한 주석을 수정.

--- src/main/java/com/zhyun/account/aop/AccountLock.java
+++ src/main/java/com/zhyun/account/aop/AccountLock.java
@@ -1,13 +1,13 @@
 package com.zhyun.account.aop;
 
-// 우와 어노테이션 생성!
+// 어노테이션 생성!
 
 import java.lang.annotation.*;
 
-@Target(ElementType.METHOD) // 메서드에 붙일 수 있음
-@Retention(RetentionPolicy.RUNTIME) // 런타임 시 붙음
-@Documented
-@Inherited  // 상송 가능
+@Target(ElementType.METHOD) // 이 어노테이션은 메서드에 붙일 수 있음
+@Retention(RetentionPolicy.RUNTIME) // 이 어노테이션은 런타임 시 붙음
+@Documented // 문서화시 내용에 꼭 포함됨
+@Inherited  // 이 어노테이션이 붙은 클래스를 상속하면 어노테이션도 상속 됨
 public @interface AccountLock {
-    long tryLockTime() default 5000L; // ?
+    long tryLockTime() default 5000L; // 이 메서드의 기본값은 5000L이다.
 }
